2bcfae800db9b9aebde0100caad680b3f136273e
Daniel LaBorde <dlabo14@lsu.edu>
2017-05-07 16:15:54 -0500

    Update simple-calc.hs

--- simple-calc.hs
+++ simple-calc.hs
@@ -1,23 +1,21 @@
 pot :: IO() -> IO() -> IO() -> IO()
-pot = 
-putStrLn "Enter the first term:"
-inputIt <- getLine
-let r1 = read inputIt :: Int
-putStrLn "Enter the next term:"
-inputIt <- getLine
-let op = inputIt
-putStrLn "Enter the next term:"
-inputIt <- getLine
-let r2 = read inputIt :: Int
-putStrLn "="
-if op == "+" then print (r1+r2) 
-             else 
-              if op == "-" then print (r1-r2) 
-             else 
-              if op == "*" then print (r1*r2) 
-             else 
-              if op == "/" then print (r1 `div` r2) else print ("nothing")
-
+pot = putStrLn "Enter the first term:"
+      inputIt <- getLine
+      let r1 = read inputIt :: Int
+      putStrLn "Enter the next term:"
+      inputIt <- getLine
+      let op = inputIt
+      putStrLn "Enter the next term:"
+      inputIt <- getLine
+      let r2 = read inputIt :: Int
+      putStrLn "="
+      if op == "+" then print (r1+r2) 
+                   else 
+                    if op == "-" then print (r1-r2) 
+                   else 
+                    if op == "*" then print (r1*r2) 
+                   else 
+                    if op == "/" then print (r1 `div` r2) else print ("nothing")
 main = do
 --let r1 = 0
 --let op = "/"
