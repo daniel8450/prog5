29f9d0c1fc20fb66dc2140a58f6ca9263dcb4416
Daniel LaBorde <dlabo14@lsu.edu>
2017-05-07 15:40:39 -0500

    Update simple-calc.hs

--- simple-calc.hs
+++ simple-calc.hs
@@ -17,7 +17,7 @@ putStrLn "Enter the next term:"
 inputIt <- getLine
 let r2 = read inputIt :: Int
 
-if op == "+" then print "=" r
+if op == "+" then putStrLn "=" ++ r
              else print r
               
                       
