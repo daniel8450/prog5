12fe9eea81207b236c7f05d444b19a18baaa7678
Daniel LaBorde <dlabo14@lsu.edu>
2017-05-07 15:36:38 -0500

    Update simple-calc.hs

--- simple-calc.hs
+++ simple-calc.hs
@@ -2,11 +2,17 @@ import Data.Function
 import Data.List
 
 
+add :: Int -> Int
+add x y = x + y
+
+subtact
+
 
 main = do
 let r1 = 0
 let op = "/"
 let r2 = 0
+let r = 0
 
 putStrLn "Enter the first term:"
 inputIt <- getLine
@@ -18,7 +24,7 @@ putStrLn "Enter the next term:"
 inputIt <- getLine
 r2 <- read inputIt :: Int
 
-if op == "+" then print (r1  op  r2 (r1+r2))
+if op == "+" then r <- r1+r2
              else print ("no")
               
                       
