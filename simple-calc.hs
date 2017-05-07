b87bf29a0e6351752a498b36fe9fbdde39418a21
Daniel LaBorde <dlabo14@lsu.edu>
2017-05-07 15:58:10 -0500

    Update simple-calc.hs

--- simple-calc.hs
+++ simple-calc.hs
@@ -7,22 +7,20 @@ let op = "/"
 let r2 = 0
 
 if (x == 0) then do
-              putStrLn "Enter the first term:"
-              inputIt <- getLine
-              let r1 = read inputIt :: Int
-              let x = 1
+                putStrLn "Enter the first term:"
+                inputIt <- getLine
+                let r1 = read inputIt :: Int
+                let x = 1
              else
-
-putStrLn "Enter the next term:"
-inputIt <- getLine
-let op = inputIt
-putStrLn "Enter the next term:"
-inputIt <- getLine
-let r2 = read inputIt :: Int
-
-putStrLn "="
-if op == "+" then print (r1+r2)
-else putStrLn "ohno"
+                putStrLn "Enter the next term:"
+                inputIt <- getLine
+                let op = inputIt
+                putStrLn "Enter the next term:"
+                inputIt <- getLine
+                let r2 = read inputIt :: Int
+                putStrLn "="
+                if op == "+" then print (r1+r2)
+                             else putStrLn "ohno"
 
 
                         
