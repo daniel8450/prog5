30dbe2e213c75dc627fa8c9541113a50fd20ff0a
Daniel LaBorde <dlabo14@lsu.edu>
2017-05-07 15:12:18 -0500

    Update simple-calc.hs

--- simple-calc.hs
+++ simple-calc.hs
@@ -5,22 +5,31 @@ import Data.List
 
 main = do
 let r1 = 0
+let op = "/"
 let r2 = 0
-let inCheck = "+"
 
-if inCheck /= "=" 
-   then do
-     putStrLn "Enter the first term:"
-     inputIt <- getLine
-     let r1 = read inputIt :: Int
-     putStrLn "Enter the next term:"
-     inputIt <- getLine
-     if (inputIt) == "+" then 
-                            print ("plus")
-                         else 
-                            let result = 500              
-    else 
-      print result
+putStrLn "Enter the first term:"
+inputIt <- getLine
+let r1 = read inputIt :: Int
+putStrLn "Enter the next term:"
+inputIt <- getLine
+if (inputIt) == "+" then let op = "+"
+                    else if inputIt == "-" then let op = "-"
+                    else if inputIt == "*" then let op = "*"
+                    else if inputIt == "/" then let op = "/"
+                    else
+                      putStrLn "Enter the next term:"
+                      inputIt <- getLine
+                      let r2 = read inputIt :: Int
+                      if op == "+" then let r = r1 + r2
+                                   else if op == "-" then let r r1 - r2
+                                   else if p == "*" then let r r1 * r2
+                                   else if p == "/" then let r r1 `div r2
+                                   else             print r
+                      
+                        
+           
+
      
 
 
