aec7ecb4e11a3176f01de6e8c38bfcccf6d122e7
Daniel LaBorde <dlabo14@lsu.edu>
2017-05-07 15:01:52 -0500

    Update simple-calc.hs

--- simple-calc.hs
+++ simple-calc.hs
@@ -6,13 +6,18 @@ import Data.List
 main = do
 let r1 = 0
 let r2 = 0
-putStrLn "Enter the first term:"
-inputIt <- getLine
-let r1 = read inputIt :: Int
-print (r1)
-putStrLn "Enter the next term:"
-inputIt <- getLine
-if (inputIt) == "+" then print ("plus")
-                    else print ("NO")
+let inCheck = "+"
+
+if inCheck != "=" 
+   then do
+     putStrLn "Enter the first term:"
+     inputIt <- getLine
+     let r1 = read inputIt :: Int
+     putStrLn "Enter the next term:"
+     inputIt <- getLine
+     if (inputIt) == "+" then print ("plus")
+                       else print ("NO")
+   else print result
+     
 
 
