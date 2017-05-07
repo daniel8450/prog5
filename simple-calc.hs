4832e3109fd621994f8da3c670baf36b884ea564
Daniel LaBorde <dlabo14@lsu.edu>
2017-05-07 15:25:38 -0500

    Update simple-calc.hs

--- simple-calc.hs
+++ simple-calc.hs
@@ -13,30 +13,13 @@ inputIt <- getLine
 let r1 = read inputIt :: Int
 putStrLn "Enter the next term:"
 inputIt <- getLine
-if (inputIt) == "+" 
-  then let op = "+"
-  else 
-    if inputIt == "-" then let op = "-"
-  else 
-    if inputIt == "*" then let op = "*"
-  else 
-    if inputIt == "/" then let op = "/"
-  else
-     putStrLn "Enter the next term:"
-     inputIt <- getLine
-     let r2 = read inputIt :: Int
-     if op == "+" 
-     then let r = r1 + r2
-     else
-       if op == "-" then let r r1 - r2
-     else
-       if p == "*"then let r r1 * r2
-     else
-       if p == "/" then let r r1 `div r2
-     else
-       print r
+let op = inputIt
+putStrLn "Enter the next term:"
+inputIt <- getLine
+let r2 = read inputIt :: Int
 
-                                 
+if op == "+" then print (r1 + r2)
+              
                       
                         
            
