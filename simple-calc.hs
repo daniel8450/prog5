42a4c5ce54f94560f5251b1bb357e3257e1282ba
Daniel LaBorde <dlabo14@lsu.edu>
2017-05-07 16:04:56 -0500

    Update simple-calc.hs

--- simple-calc.hs
+++ simple-calc.hs
@@ -11,18 +11,18 @@ if x == 0 then do
             inputIt <- getLine
             let r1 = read inputIt :: Int
             let x = 1
-else
-putStrLn "Enter the next term:"
-inputIt <- getLine
-let op = inputIt
-putStrLn "Enter the next term:"
-inputIt <- getLine
-let r2 = read inputIt :: Int
-putStrLn "="
-if op == "+" then print (r1+r2) else putStrLn "ohno"
-if op == "-" then print (r1-r2) else putStrLn "ohno"
-if op == "*" then print (r1*r2) else putStrLn "ohno"
-if op == "/" then print (r1 `div` r2) else putStrLn "ohno"
+          else
+            putStrLn "Enter the next term:"
+            inputIt <- getLine
+            let op = inputIt
+            putStrLn "Enter the next term:"
+            inputIt <- getLine
+            let r2 = read inputIt :: Int
+            putStrLn "="
+            if op == "+" then print (r1+r2) else putStrLn "ohno"
+            if op == "-" then print (r1-r2) else putStrLn "ohno"
+            if op == "*" then print (r1*r2) else putStrLn "ohno"
+            if op == "/" then print (r1 `div` r2) else putStrLn "ohno"
 
                         
            
