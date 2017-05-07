aae44fe57fb2c20bc8b339031e700cf0bce8ff52
Daniel LaBorde <dlabo14@lsu.edu>
2017-05-07 15:25:59 -0500

    Update simple-calc.hs

--- simple-calc.hs
+++ simple-calc.hs
@@ -19,6 +19,7 @@ inputIt <- getLine
 let r2 = read inputIt :: Int
 
 if op == "+" then print (r1 + r2)
+             else print (no)
               
                       
                         
